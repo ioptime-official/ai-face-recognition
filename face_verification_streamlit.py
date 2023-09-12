f172fa491953e8ad1e9958028ac05bc3c96e1956
Sharjeel Khan <108624605+RaoSharjeelKhan@users.noreply.github.com>
2023-09-12 16:20:59 +0500

    Update face_verification_streamlit.py

--- face_verification_streamlit.py
+++ face_verification_streamlit.py
@@ -22,7 +22,6 @@ if uploaded_images_1:
         bgr_image1 = cv2.cvtColor(image1_array, cv2.COLOR_RGB2BGR)
         face_1=DeepFace.extract_faces(bgr_image1 , detector_backend='mtcnn')
         roi_1 = face_1[0]['face']
-        #roi_2  = cv2.cvtColor(roi_1 , cv2.COLOR_BGR2RGB)
         face_image1_uint8 = (roi_1 * 255).astype(np.uint8) 
         image_1=cv2.resize(face_image1_uint8, (roi_1.shape[0],roi_1.shape[1]))
         with mp_face_mesh.FaceMesh(static_image_mode=True) as face_mesh:
@@ -38,13 +37,7 @@ if uploaded_images_1:
                 face_extracted = cv2.bitwise_and(image_1, image_1, mask=mask)
                 gamma_corrected_channels = [np.power(face_extracted[:, :, c] / 255.0, gamma) * 255.0 for c in range(3)]
                 face_extracted= np.stack(gamma_corrected_channels, axis=-1).astype(np.uint8)
-                # yuv_image = cv2.cvtColor(face_extracted, cv2.COLOR_BGR2YUV)
-                # yuv_image[:,:,0] = cv2.equalizeHist(yuv_image[:,:,0])
-                # face_extracted = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2BGR)
-
                 st.image(face_extracted, caption='Uploaded Image 1', use_column_width=True)
-
-
 
     except:
         st.warning("Face is not detected in the uploaded image. Kindly use different image")
@@ -61,7 +54,6 @@ if uploaded_images_2:
         bgr_image2 = cv2.cvtColor(image2_array, cv2.COLOR_RGB2BGR)
         face_2=DeepFace.extract_faces(bgr_image2, detector_backend='mtcnn')
         roi_2 = face_2[0]['face']
-        #roi_2  = cv2.cvtColor(roi_2 , cv2.COLOR_BGR2RGB)
         face_image2_uint8 = (roi_2 * 255).astype(np.uint8) 
         image_2=cv2.resize(face_image2_uint8, (roi_2.shape[0],roi_2.shape[1]))
         with mp_face_mesh.FaceMesh(static_image_mode=True) as face_mesh:
@@ -76,18 +68,12 @@ if uploaded_images_2:
                 cv2.fillConvexPoly(mask, hull, 255)
                 face_extracted_2 = cv2.bitwise_and(image_2, image_2, mask=mask)
                 gamma_corrected_channels2 = [np.power(face_extracted_2[:, :, c] / 255.0, gamma) * 255.0 for c in range(3)]
-                face_extracted_2= np.stack(gamma_corrected_channels2, axis=-1).astype(np.uint8)
-                # yuv_image2 = cv2.cvtColor(face_extracted_2, cv2.COLOR_BGR2YUV)
-                # yuv_image2[:,:,0] = cv2.equalizeHist(yuv_image2[:,:,0])
-                # face_extracted_2 = cv2.cvtColor(yuv_image2, cv2.COLOR_YUV2BGR)
-
-                
+                face_extracted_2= np.stack(gamma_corrected_channels2, axis=-1).astype(np.uint8)            
                 st.image(face_extracted_2 , caption='Uploaded Image 1', use_column_width=True)
 
     except:
         st.warning("Face is not detected in the uploaded image. Kindly use different image")
 
-# Load your TFLite model for face embeddings
 tflite_model_path = 'arcface_model_quant.tflite'
 interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
@@ -101,9 +87,7 @@ def get_embedding(image):
     input_image = np.expand_dims(input_image, axis=0)
     interpreter.set_tensor(input_details[0]['index'], input_image)
     interpreter.invoke()
-    output_tensor = interpreter.get_tensor(output_details[0]['index'])
-    # normalized_embedding = output_tensor[0] / np.sqrt(np.sum(output_tensor[0]**2))
-    
+    output_tensor = interpreter.get_tensor(output_details[0]['index'])    
     return output_tensor[0] 
 
 def get_embedding(image):
@@ -117,17 +101,8 @@ def get_embedding(image):
 
 if st.button("Verify"):
     if 'image1' in locals() and 'image2' in locals():
-        # obj=DeepFace.verify(face_extracted, face_extracted_2, model_name="ArcFace", detector_backend='mtcnn')
-        # st.write((1-obj['distance'])*100)
-        # if ((1-obj['distance'])*100) >= 40:
-        #     st.write('Face Matched')
-        # else:
-        #     st.write("Face is not Matched")
-
         embedding_first = get_embedding(face_extracted/255)
         embedding_second = get_embedding(face_extracted_2/255)
-       # st.image(face_extracted , caption='Uploaded Image 1', use_column_width=True)
-        #st.image(face_extracted_2 , caption='Uploaded Image 1', use_column_width=True)
         cosine_similarity = dst.findCosineDistance(embedding_first, embedding_second)
         st.write(f"Cosine Similarity Score: {(1-cosine_similarity)*100:.2f}")
     else:
